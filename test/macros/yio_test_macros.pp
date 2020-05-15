ffb7e3f117da41c290f251a29339cae90273b6b9
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-05-15 03:42:08 +0200

    fix(test): Add macro expansion test stub

---
+++ test/macros/yio_test_macros.pp
@@ -0,0 +1,11 @@
+/**
+ * @file yio_test_macros.pp
+ * @date 2020-05-14
+ * @author Kamil Cukrowski
+ * @copyright 
+ * SPDX-License-Identifier: GPL-3.0-only
+ * @brief
+ */
+#include <yio_test_private.h>
+
+// PASS_REGULAR_EXPRESSION yio_test_private.h
