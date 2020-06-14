452f6d7253c560d2763107b9b963c8371402f3ca
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-06-14 03:49:02 +0200

    feat: Add yio_stupiddfp library

---
+++ src/yio/stupiddfp/yio_stupiddfp.c
@@ -0,0 +1,127 @@
+/**
+ * @file yio_stupiddfp.c
+ * @date 2020-06-11
+ * @author Kamil Cukrowski
+ * @copyright 
+ * SPDX-License-Identifier: GPL-3.0-only
+ * @brief
+ */
+#define __STDC_WANT_IEC_60559_DFP_EXT__
+m4_config();
+#include "yio_stupiddfp.h"
+#include <yio/yio_config.h>
+#include <stdbool.h>
+#include <math.h>
+
+#if __STDC_IEC_60559_DFP__ > 0
+#define _yIO_HAVE_IEC_60559_DFP
+#endif
+
+m4_applyforeachdefine(
+`(
+		( 32, df,  "H"),
+		( 64, dd,  "D"),
+		(128, dl, "DD")
+)~,`m4_dnl;
+
+#define _yIO_FLOAT_Cd$1(x)  x ## $2
+
+/* suffix $1 ---------------------------------------------------- */
+
+#ifdef _yIO_HAVE_DEC$1
+#ifdef _yIO_HAVE_IEC_60559_DFP
+
+_Decimal$1 frexp10d$1(_Decimal$1 val, int *exp) {
+	return frexpd$1(val, exp);
+}
+
+#else // _yIO_HAVE_IEC_60559_DFP
+
+_Decimal$1 floord$1(_Decimal$1 x) {
+	return (int)x;
+}
+
+_Decimal$1 fabsd$1(_Decimal$1 x) {
+	return x > 0 ? x : -x;
+}
+
+_Decimal$1 _yIO_expintXd$1(_Decimal$1 y, _Decimal$1 X) {
+	if (y == 0) {
+		return 1;
+	}
+
+	const bool negative = y < 0;
+	if (negative) {
+		y = fabsd$1(y);
+	}
+
+	_Decimal$1 res = X;
+	while (--y > 0) {
+		res *= X;
+	}
+
+	return negative ? 1 / res : res;
+}
+
+_Decimal$1 exp2d$1(_Decimal$1 y) {
+	return _yIO_expintXd$1(y, 2);
+}
+
+
+_Decimal$1 exp10d$1(_Decimal$1 y) {
+	return _yIO_expintXd$1(y, 10);
+}
+
+_Decimal$1 roundd$1(_Decimal$1 y) {
+	y += _yIO_FLOAT_Cd$1(0.5);
+	return y - (y - (int)y);
+}
+
+_Decimal$1 log2d$1(_Decimal$1 x) {
+	// https://stackoverflow.com/questions/39864840/using-series-to-approximate-log2
+	unsigned long n = 1;
+	_Decimal$1 eps = _yIO_FLOAT_Cd$1(0.00001);
+	_Decimal$1 kpow = (x - 1) / (x + 1);
+	_Decimal$1 kpow2 = kpow * kpow;
+	_Decimal$1 dk;
+	_Decimal$1 k = 2 * kpow;
+
+	do {
+	    n += _yIO_FLOAT_Cd$1(2.0);
+	    kpow *= kpow2;
+	    dk = _yIO_FLOAT_Cd$1(2.0) * kpow / n;
+	    k += dk;
+	} while (fabsd$1(dk) >= eps);
+
+	return dk;
+}
+
+_Decimal$1 log10d$1(_Decimal$1 y) {
+	static const _Decimal$1 log2_of_10_d$1 = _yIO_FLOAT_Cd$1(
+			2.30258509299404568401799145468436420760110148862877297603332790096
+	);
+	return log2d$1(y) / log2_of_10_d$1;
+}
+
+_Decimal$1 frexpd$1(_Decimal$1 val, int *exp) {
+	if (val == 0) {
+		*exp = 0;
+	} else {
+		const int tmp = 1 + floord$1(log10d$1(val));
+		*exp = tmp;
+		val *= exp10d$1(-tmp);
+		if (val < _yIO_FLOAT_Cd$1(0.1)) {
+			val = _yIO_FLOAT_Cd$1(0.1);
+		} else if (val > _yIO_FLOAT_Cd$1(1.0)) {
+			val = _yIO_FLOAT_Cd$1(1.0);
+		}
+	}
+	return val;
+}
+
+#endif // _yIO_HAVE_IEC_60559_DFP
+
+#endif // _yIO_HAVE_DEC$1
+
+~) // m4_dnl m4_applyforeachdefine ;
+
---
+++ src/yio/stupiddfp/yio_stupiddfp.h
@@ -0,0 +1,39 @@
+/**
+ * @file yio_stupiddfp.h
+ * @date 2020-06-11
+ * @author Kamil Cukrowski
+ * @copyright 
+ * SPDX-License-Identifier: GPL-3.0-only
+ * @brief
+ */
+#pragma once
+m4_config();
+#include <yio/yio_config.h>
+
+#if __CDT_PARSER__
+#define _yIO_HAVE_DEC$1
+#define _Decimal$1 _Decimal32
+#endif
+
+m4_applyforeachdefine(
+`(
+		( 32, df,  "H"),
+		( 64, dd,  "D"),
+		(128, dl, "DD")
+)~,`m4_dnl;
+
+#ifdef _yIO_HAVE_DEC$1
+
+_Decimal$1 floord$1(_Decimal$1 x);
+_Decimal$1 fabsd$1(_Decimal$1 x);
+_Decimal$1 _yIO_expintXd$1(_Decimal$1 y, _Decimal$1 X);
+_Decimal$1 exp2d$1(_Decimal$1 y);
+_Decimal$1 exp10d$1(_Decimal$1 y);
+_Decimal$1 roundd$1(_Decimal$1 y);
+_Decimal$1 log2d$1(_Decimal$1 x);
+_Decimal$1 log10d$1(_Decimal$1 y);
+_Decimal$1 frexpd$1(_Decimal$1 val, int *exp);
+
+#endif // _yIO_HAVE_DEC$1
+
+~) // m4_dnl m4_applyforeachdefine ;
