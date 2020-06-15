ef6cfe11d34c47639c274c10dadf773d750db5c0
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-06-15 13:10:42 +0200

    refactor(cmake): Add labels extraction from tests

--- cmake/test_extract_properties_from_file.cmake
+++ cmake/test_extract_properties_from_file.cmake
@@ -8,20 +8,38 @@
 # would be executed. The purpouse is to store the regular expressions
 # inside the file.
 function(test_extract_properties_from_file testname file)
+
+	# Exactly one space after slashes
 	set(prefix "[ \t]*// ")
+	
 	foreach(i IN ITEMS
 		PASS_REGULAR_EXPRESSION
 		FAIL_REGULAR_EXPRESSION
 		SKIP_REGULAR_EXPRESSION
 		WILL_FAIL
+		LABELS
 	)
 		file(STRINGS ${file} expr REGEX "^${prefix}${i} ")
 		if(expr)
 			string(REGEX REPLACE "^${prefix}${i} " "" expr "${expr}")
-			if("${expr}" MATCHES  ";${prefix}${i} " AND ("${expr}" MATCHES ".^" OR "${expr}" MATCHES "$."))
-				message(FATAL_ERROR "Problem with item ${i} in ${file}")
+			
+			if(${i} STREQUAL "LABELS")
+				# labels are appended
+				set_property(TESTS ${testname} APPEND PROPERTY ${i} ${expr})
+				continue()
 			endif()
-			string(REGEX REPLACE ";${prefix}${i} " "[^\n]*\n" expr "${expr}")
+
+			if(${i} MATCHES "_REGULAR_EXPRESSION")		
+				# Because regular expressions are concatenated in set_tests_properties
+				# dissallow the use of `^` and `$` anchors.
+				# The check is only done if there are multiple prefixes.
+				if("${expr}" MATCHES ";${prefix}${i} " AND 
+						("${expr}" MATCHES ".^" OR "${expr}" MATCHES "\$."))
+					message(FATAL_ERROR "Problem with item ${i} in ${file}")
+				endif()
+				string(REGEX REPLACE ";${prefix}${i} " "[^\\\\n]*\n" expr "${expr}")
+			endif()
+			
 			set_tests_properties(${testname} PROPERTIES ${i} "${expr}")
 		endif()
 	endforeach()
