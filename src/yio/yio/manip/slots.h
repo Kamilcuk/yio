3f801747a628b6877c764555b73f08099b88b854
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-06-24 11:44:44 +0200

    yio:slots.h: Fix commments
    Do not generate ton of comments in output - one comment
    is enough. Also remove additional @def.

--- src/yio/yio/manip/slots.h
+++ src/yio/yio/manip/slots.h
@@ -8,22 +8,42 @@
  */
 m4_config(yio);
 
-m4_applyforloopdefine(100, m4_SLOTS_END, `m4_dnl;
 /**
- * @def YIO_PRINT_SLOT_$1()
- *
+ * @def YIO_PRINT_SLOT_100
  * User define-able macro that can be used to provide
- * an user overload for specific types for scanning.
+ * an user overload for specific types.
+ * To overflow on a type use a part of @b _Generic() syntax.
+ * For example if you want to provide a custom overflow
+ * for some custom type, it could look like this:
+ *
+ *      #define YIO_PRINT_SLOT_101() struct A: print_struct_a,
+ *      #include <yio.h>
+ *
+ *      struct A {
+ *         int a;
+ *         int b;
+ *      };
+ *      int print_struct_a(yio_printctx_t *ctx) {
+ *          struct A printme = yio_printctx_va_arg(t, struct A);
+ *          return yio_printctx_print(printme.a, ", ", printme.b);
+ *      }
+ *      int main() {
+ *         struct A a = { 1, 2 };
+ *         yprint(a); // will print "1, 2"
+ *      }
+ *
  */
+/**
+ * @def YIO_SCAN_SLOT_100
+ * User define-able macro that can be used to provide
+ * an user overload for specific types.
+ * @see YIO_PRINT_SLOT_$1
+ */
+
+m4_applyforloopdefine(100, m4_SLOTS_END, `m4_dnl;
 #ifndef YIO_PRINT_SLOT_$1
 #define YIO_PRINT_SLOT_$1()
 #endif
-/**
- * @def YIO_SCAN_SLOT_$1()
- *
- * User define-able macro that can be used to provide
- * an user overload for specific types for scanning.
- */
 #ifndef YIO_SCAN_SLOT_$1
 #define YIO_SCAN_SLOT_$1()
 #endif
@@ -31,14 +51,12 @@ m4_applyforloopdefine(100, m4_SLOTS_END, `m4_dnl;
 
 
 /**
- * @def _yIO_PRINT_FUNC_GENERIC_SLOTS()
  * Expanded all slots macros.
  */
 #define _yIO_PRINT_FUNC_GENERIC_SLOTS() \
 		m4_applyforloopdefine(100, m4_SLOTS_END, `YIO_PRINT_SLOT_$1()~)
 
 /**
- * @def _yIO_SCAN_FUNC_GENERIC_SLOTS()
  * Expanded all slots macros.
  */
 #define _yIO_SCAN_FUNC_GENERIC_SLOTS() \
