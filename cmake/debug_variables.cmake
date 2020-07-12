6c8129660fe132a1a6ba16ab9c293f529b509944
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-07-12 12:30:45 +0200

    cmake:debug_variables: Fix testing flags

--- cmake/debug_variables.cmake
+++ cmake/debug_variables.cmake
@@ -7,24 +7,29 @@
 ##
 ## Outputs all variables passed to it with values.
 function(debug_variables)
-	cmake_parse_arguments(_debug_variables "DV_ONCE;DV_NOSORT" "DV_PREFIX;DV_LEVEL" "" ${ARGN})
+	cmake_parse_arguments(_debug_variables "DV_ONCE;DV_NOSORT;DV_TRACE" "DV_PREFIX;DV_LEVEL" "" ${ARGN})
 	
-	if(DEFINED _debug_variables_DV_ONCE)
+	if(_debug_variables_DV_ONCE)
 		string(MD5 _debug_variables_var "${PROJECT_NAME} ${CMAKE_CURRENT_LIST_FILE} ${CMAKE_CURRENT_LIST_LINE}")
 		string(PREPEND _debug_variables_var "_debug_variables_")
 		if(DEFINED ${_debug_variables_var} AND ${_debug_variables_var})
+			# message(STATUS "${_debug_variables_var}=${${_debug_variables_var}}")
 			return()
 		endif()
 		set("${_debug_variables_var}" TRUE CACHE INTERNAL "" FORCE)
 	endif()
+	unset(_debug_variables_DV_ONCE)
 
 	if(NOT DEFINED _debug_variables_DV_LEVEL)
 		set(_debug_variables_DV_LEVEL STATUS)
 	endif()
+	if(_debug_variables_DV_TRACE)
+		set(_debug_variables_DV_LEVEL TRACE)
+	endif()
 
 	set(_debug_variables_list "${_debug_variables_UNPARSED_ARGUMENTS}")
 
-	if(NOT DEFINED _debug_variables_DV_NOSORT)
+	if(NOT _debug_variables_DV_NOSORT)
 		list(SORT _debug_variables_list)
 	endif()
 
