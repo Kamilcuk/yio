d4012ab476c5cf60d6c4b58c07f702da7980296f
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-07-16 03:39:54 +0200

    cmake: Sort out too many spaces when compiling
    Also move setting CROSSCOMPILING_EMULATOR into toolchain file

--- cmake/Toolchain/arm-none-eabi-gcc.cmake
+++ cmake/Toolchain/arm-none-eabi-gcc.cmake
@@ -6,21 +6,20 @@ set(CMAKE_SYSTEM_PROCESSOR ARM)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-set(_common_flags " -ffunction-sections -fdata-sections ")
-set(CMAKE_C_FLAGS_INIT " ${_common_flags} ")
-set(CMAKE_EXE_LINKER_FLAGS_INIT " ${_common_flags} ")
-string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT 
-	" -Wl,--gc-sections --specs=rdimon.specs -u_printf_float -u_scanf_float ")
+set(_common_flags " -ffunction-sections -fdata-sections")
+set(CMAKE_C_FLAGS_INIT " ${_common_flags}")
+set(CMAKE_EXE_LINKER_FLAGS_INIT " ${_common_flags}")
 unset(_common_flags)
+string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT
+	" -Wl,--gc-sections --specs=rdimon.specs -u_printf_float -u_scanf_float")
 
-set(CMAKE_C_FLAGS_DEBUG_INIT " -ggdb3")
-set(CMAKE_EXE_LINKER_FLAGS_DEBUG "")
-
-set(CMAKE_C_FLAGS_RELEASE_INIT " -Os")
-set(CMAKE_EXE_LINKER_FLAGS_RELEASE "")
+string(APPEND CMAKE_C_FLAGS " -Os")
 
 find_program(CMAKE_ASM_COMPILER  arm-none-eabi-gcc)
 find_program(CMAKE_C_COMPILER    arm-none-eabi-gcc)
 find_program(CMAKE_CXX_COMPILER  arm-none-eabi-g++)
 
 include(${CMAKE_CURRENT_LIST_DIR}/crosscompiling.cmake)
+
+get_filename_component(CMAKE_CROSSCOMPILING_EMULATOR
+	${CMAKE_CURRENT_LIST_DIR}/../../scripts/cmake_crosscompiling_emulator_arm_none_gdb.sh ABSOLUTE)
