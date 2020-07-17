b096be202096f1f04b1bf83de4abd731080756bd
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-07-17 15:44:33 +0200

    scripts: Do not use cp, use rsync only

--- scripts/syncdir.sh
+++ scripts/syncdir.sh
@@ -26,21 +26,11 @@ fi
 
 if (($# == 1)); then
 	mkdir -p "$1"
-	if hash rsync >/dev/null 2>&1; then
-		rsync --delete --files-from=/dev/stdin $verboseflag -a . "$1"
-	else 
-		rm -rf "$1"/*
-		xargs -t -d'\n' cp --parents $verboseflag -a -t "$1"
-	fi
+	rsync --delete --files-from=/dev/stdin $verboseflag -a . "$1"
 else
 	dir="${@: -1}"
 	mkdir -p "$dir"
-	if hash rsync >/dev/null 2>&1; then
-		rsync --delete $verboseflag -a "$@"
-	else
-		rm -rf "$dir"/*
-		cp --parents $verboseflag -a "$@"
-	fi
+	rsync --delete $verboseflag -a "$@"
 fi
 
 
