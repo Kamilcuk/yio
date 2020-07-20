8037dcef03462a89493cfa21eac653e70ae0be81
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-07-21 01:57:32 +0200

    Allow for custom modification of float printing to choose anything.

--- src/configuration.cmake
+++ src/configuration.cmake
@@ -14,16 +14,10 @@ set(YIO_CHECK_VA_ARG_SIZES ${tmp} CACHE BOOL
 	"Enabeld by default in debugging builds")
 
 set(YIO_FLOATS_PREFER_CUSTOM 0 CACHE BOOL
-	"For managing standard floating point numbers, prefer using custom stupid library")
+	"For managing standard floating point numbers, 
+	if strfrom* functions is not available,
+	prefer using custom stupid library over printf function")
 var_to_0_or_1(YIO_FLOATS_PREFER_CUSTOM)
-
-set(YIO_IEC_60559_BFP_PREFER_CUSTOM 0 CACHE BOOL
-	"For managing IEC 60559 binary floating point numbers _FloatN, prefer using custom stupid library")
-var_to_0_or_1(YIO_IEC_60559_BFP_PREFER_CUSTOM)
-
-set(YIO_IEC_60559_DFP_PREFER_CUSTOM 0 CACHE BOOL
-	"For managing IEC 60559 decimal floating point numbers _DecimalN, prefer using custom stupid library")
-var_to_0_or_1(YIO_IEC_60559_DFP_PREFER_CUSTOM)
 
 if(_yIO_HAS_UNISTD_H)
 cmake_dependent_option(YIO_USE_INPUT_FD 1 CACHE BOOL
--- src/yio/yio/manip/print_float.h
+++ src/yio/yio/manip/print_float.h
@@ -17,6 +17,10 @@ m4_applyforeachdefine(`(
 		(l, long double)
 )~,`m4_dnl;
 
+#define YIO_PRINT_FLOATS_WITH_STRFROM  1
+#define YIO_PRINT_FLOATS_WITH_PRINTF   2
+#define YIO_PRINT_FLOATS_WITH_CUSTOM   3
+
 #ifdef _yIO_HAS_FLOAT$1
 
 int _yIO_print_float_strfrom$1(yio_printctx_t *t);
@@ -25,6 +29,18 @@ int _yIO_print_float_printf$1(yio_printctx_t *t);
 
 static inline
 int _yIO_print_float$1(yio_printctx_t *t) {
+#ifdef YIO_PRINT_FLOATS_WITH
+#if YIO_PRINT_FLOATS_WITH == YIO_PRINT_FLOATS_WITH_STRFROM
+	return _yIO_print_float_strfrom$1(t);
+#elif YIO_PRINT_FLOATS_WITH == YIO_PRINT_FLOATS_WITH_PRINTF
+	return _yIO_print_float_printf$1(t);
+#elif YIO_PRINT_FLOATS_WITH == YIO_PRINT_FLOATS_WITH_CUSTOM
+	return _yIO_print_float_stupid$1(t);
+#else
+#error "Invalid value for YIO_PRINT_FLOATS_WITH. It must be set to one of YIO_PRINT_FLOATS_WITH_* macros"
+#endif
+#else // YIO_PRINT_FLOATS_WITH
+	// If no printing function was specified, use defaults
 #ifdef _yIO_HAS_strfrom$1
 	return _yIO_print_float_strfrom$1(t);
 #else // _yIO_HAS_strfrom$1
@@ -34,7 +50,9 @@ int _yIO_print_float$1(yio_printctx_t *t) {
 	return _yIO_print_float_printf$1(t);
 #endif // YIO_FLOATS_PREFER_CUSTOM
 #endif // _yIO_HAS_strfrom$1
+#endif // YIO_PRINT_FLOATS_WITH
 }
+
 #define _yIO_PRINT_FUNC_GENERIC_FLOAT$1()  \
 		$2: _yIO_print_float$1,
 
