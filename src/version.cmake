dfe7115ad961530456a6b8565b1126594e5e91a7
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-07-20 11:42:17 +0200

    refactor(cmake): Pad version hex with initial zeros so it looks nicely

--- src/version.cmake
+++ src/version.cmake
@@ -34,6 +34,21 @@ set(YIO_PROJECT_VERSION "${CMAKE_MATCH_1}.${CMAKE_MATCH_2}.${CMAKE_MATCH_3}.${CM
 math(EXPR YIO_VERSION "(${CMAKE_MATCH_1} << 24) | (${CMAKE_MATCH_2} << 16) | ${CMAKE_MATCH_3} << 8 | ${CMAKE_MATCH_4}" OUTPUT_FORMAT HEXADECIMAL)
 set(YIO_VERSION_STRING "${_output}")
 
+# change "0xabc" to "0x00000abc"
+function(pad_version_with_zeros_to_8_characters var val)
+	string(REGEX REPLACE "^0x" "" "val" "${val}")
+	while(TRUE)
+		string(LENGTH "${val}" _tmp)
+		if(_tmp LESS 8)
+			string(PREPEND val "0")
+		else()
+			break()
+		endif()
+	endwhile()
+	set(${var} "0x${val}" PARENT_SCOPE)
+endfunction()
+pad_version_with_zeros_to_8_characters(YIO_VERSION "${YIO_VERSION}")
+
 yio_debug_variables(
 	YIO_PROJECT_VERSION
 	YIO_VERSION
