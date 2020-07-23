e521a9c1778cbf64066ed6e28a4a841228dc789c
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-07-23 12:08:40 +0200

    style: Print yio_debug_variables once

--- cmake/yio_debug_variables.cmake
+++ cmake/yio_debug_variables.cmake
@@ -5,7 +5,18 @@ function(yio_debug_variables)
 	if(NOT YIO_DEV)
 		return()
 	endif()
-	
+
+	if($ENV{YIO_DEBUG_VARIABLES_ALWAYS})
+	else()
+		string(MD5 varname "${ARGV}")
+		string(PREPEND varname "YIO_DEBUG_VARIABLES_")
+		if(${varname})
+			return()
+		endif()
+		set(${varname} TRUE CACHE INTERNAL
+			"Internal variable used to make yio_debug_variable print only once")
+	endif()
+
 	set(_yio_list "${ARGV}")
 
 	list(SORT _yio_list)
@@ -18,6 +29,5 @@ function(yio_debug_variables)
 			set(_yio_val " undefined")
 		endif()
 		message(STATUS "yio: ${_yio_i}${_yio_val}")
-	endforeach()	
+	endforeach()
 endfunction()
-
