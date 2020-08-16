724aad47bdee80139fb79a44d70fc80740b95f53
Kamil Cukrowski <kamilcukrowski@gmail.com>
2020-08-17 01:52:13 +0200

    scripts: Add compile_test_run script

---
+++ scripts/compile_test_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "$BASH_SOURCE")"/..
+tmp=$(mktemp)
+trap 'rm "$tmp"' EXIT
+{
+	printf '#include <%s.h>\n' yio ywio yuio
+	cat
+} |
+gcc -L_build/Debug/src -Wl,--whole-archive $(
+	find _build/Debug/src -maxdepth 1 -name '*.a' -type f -printf "%f\n" |
+	sed 's/lib/-l/;s/\.a//'
+) -lunistring -lm -Wl,--no-whole-archive -I$(readlink -f gen) -o "$tmp" -xc - && "$tmp"
+
