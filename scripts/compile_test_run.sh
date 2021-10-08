b6021ea4d4550edf5398e868d558d295e22cf6a6
Kamil Cukrowski <kamilcukrowski@gmail.com>
2021-10-08 19:02:25 +0200

    Add ydebug header

--- scripts/compile_test_run.sh
+++ scripts/compile_test_run.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+set -euo pipefail
 
-cd "$(dirname "$BASH_SOURCE")"/..
+cd "$(dirname "${BASH_SOURCE[0]}")"/..
+gen="./gen"
+libs=$(
+	find _build/Debug/src -maxdepth 1 -name '*.a' -type f -printf "%f\n" |
+	sed 's/lib/-l/;s/\.a//'
+)
+
 tmp=$(mktemp)
 trap 'rm "$tmp"' EXIT
 {
 	printf '#include <%s.h>\n' yio ywio yuio
-	cat
-} |
-gcc -L_build/Debug/src -Wl,--whole-archive $(
-	find _build/Debug/src -maxdepth 1 -name '*.a' -type f -printf "%f\n" |
-	sed 's/lib/-l/;s/\.a//'
-) -lunistring -lm -Wl,--no-whole-archive -I$(readlink -f gen) -o "$tmp" -xc - && "$tmp"
+	if (($#)); then
+		printf "%s\n" "$@"
+	else
+		cat
+	fi
+} | {
+	set -x
+	gcc -L_build/Debug/src -Wall -Wl,--whole-archive $libs -lunistring -lm -Wl,--no-whole-archive -I"$gen" -o "$tmp" -xc -
+} && "$tmp"
 
---
+++ src/yio/ydebug.h
@@ -0,0 +1,42 @@
+/**
+ * @file
+ * @date 2020-06-15
+ * @author Kamil Cukrowski
+ * @copyright
+ * SPDX-License-Identifier: GPL-3.0-only
+ * @brief
+ */
+#ifndef YIO_YIO_DEBUG_H_
+#define YIO_YIO_DEBUG_H_
+#include "yio.h"
+
+#if __GNUC__
+#  define _yIO_DEBUG_FUNCTION	__extension__ __PRETTY_FUNCTION__
+#else
+# if defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
+#  define _yIO_DEBUG_FUNCTION	__func__
+# else
+#  define _yIO_DEBUG_FUNCTION	((const char *) 0)
+# endif
+#endif
+
+m4_applyforloopdefine(1, m4_MLVLS, `m4_dnl;
+#define _yIO_ydebug_$1(m4_seqdashcomma(1, $1)) \
+			m4_forloopdashX(1, $1,
+			`#X"=", X~,
+			`, " ", \
+			~)
+~)m4_dnl;
+#define _yIO_ydebug(m4_seqdashcomma(1, m4_MLVLS), N, ...)  \
+		_yIO_ydebug_##N
+
+
+#define ydebug(...) \
+	yfprint(stderr, \
+			__FILE__, (char)':', __LINE__, (char)':', _yIO_DEBUG_FUNCTION, (char)':', (char)' ', \
+			_yIO_ydebug(__VA_ARGS__, m4_seqcomma(m4_MLVLS, 0))(__VA_ARGS__), (char)'\n')
+#define ydebugil(...)  \
+	yfprint(stderr, \
+			_yIO_ydebug(__VA_ARGS__, m4_seqcomma(m4_MLVLS, 0))(__VA_ARGS__), (char)'\n')
+
+#endif
