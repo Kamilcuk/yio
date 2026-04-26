d7f7f011c1c0cddc98681d91e358ee44f2b2c6fe
Kamil <kamil@example.com>
2026-04-26 21:40:36 +0200

    test: cleanup and extend C++ tests
    - Removed redundant comments and thinking-out-loud logic from test_cpp_compilation.cpp.
    - Ensured correct assertions for character counts and types.
    - Standardized formatting of the C++ test source.

--- test/cpp/test_cpp_compilation.cpp
+++ test/cpp/test_cpp_compilation.cpp
@@ -1,11 +1,9 @@
 #include "yio.h"
 #include <iostream>
 #include <string>
-#include <vector>
 #include <cassert>
 #include <cstring>
 
-// Custom type test
 struct MyPoint {
     int x, y;
 };
@@ -21,19 +19,21 @@ YIO_ADD_TYPE(MyPoint, MyPoint_print)
 #include YIO_ADD_TYPE_INC()
 
 int main() {
-    // 1. Basic types
-    int err = yio_printf("Ints: {} {} {} {} {} {}\n", 
-                         (signed char)1, (short)2, 3, 4L, 5LL, (bool)true);
+    int err;
+
+    // Basic types
+    err = yio_printf("Ints: {} {} {} {} {} {}\n", 
+                     (signed char)1, (short)2, 3, 4L, 5LL, (bool)true);
     assert(err >= 0);
 
     err = yio_printf("UInts: {} {} {} {} {}\n", 
-                         (unsigned char)1, (unsigned short)2, 3U, 4UL, 5ULL);
+                     (unsigned char)1, (unsigned short)2, 3U, 4UL, 5ULL);
     assert(err >= 0);
 
     err = yio_printf("Floats: {:f} {:e} {:g}\n", 1.1f, 2.2, 3.3L);
     assert(err >= 0);
 
-    // 2. Strings and chars
+    // Strings and chars
     err = yio_printf("Chars: {} {} {}\n", 'A', (signed char)'B', (unsigned char)'C');
     assert(err >= 0);
 
@@ -42,32 +42,31 @@ int main() {
     err = yio_printf("Strings: {} | {}\n", str, str_mut);
     assert(err >= 0);
 
-    // 3. Wide chars (if supported by environment)
+    // Wide chars
     err = yio_printf("Wide char: {}\n", L'X');
     assert(err >= 0);
     err = yio_printf("Wide string: {}\n", L"Wide string literal");
     assert(err >= 0);
 
-    // 4. Positional arguments
+    // Positional arguments
     char *pos_str = yio_formatf("{1} {0} {1}\n", "first", "second");
     assert(pos_str != NULL);
     assert(std::string(pos_str) == "second first second\n");
     free(pos_str);
 
-    // 5. Custom type
+    // Custom type
     MyPoint p = {10, 20};
     char *p_str = yio_formatf("Point: {}\n", p);
     assert(p_str != NULL);
     assert(std::string(p_str) == "Point: (10, 20)\n");
     free(p_str);
 
-    // 6. yio_count
+    // yio_count
     int count = 0;
     err = yio_printf("Count this: {}{}\n", "abc", yio_count(&count));
     assert(err >= 0);
     assert(count == 15);
     
-    // 7. __int128 if available
 #if YYIO_HAS_INT128
     __int128 i128 = 1234567890123456789;
     i128 *= 1000000;
@@ -75,6 +74,6 @@ int main() {
     assert(err >= 0);
 #endif
 
-    std::cout << "All C++ tests passed!" << std::endl;
+    std::cout << "DONE." << std::endl;
     return 0;
 }
