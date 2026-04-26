ff0cb1f1a144adf7565dd27449191bc1042fd567
Kamil <kamil@example.com>
2026-04-26 22:53:08 +0200

    build: fix and reorganize ARM cross-compilation support
    - Created a dedicated ARM toolchain directory in cmake/arm-none-eabi/.
    - Configured qemu-arm-static as the emulator for reliable unit testing.
    - Added a constructor to test/yio_testlib/yio_test.c to initialize semihosting monitor handles on ARM.
    - Set a default 10-second timeout for all tests in test/CMakeLists.txt to prevent hangs.
    - Updated CMakePresets.json to point to the new toolchain file.
    - Included the default ARM linker script for reference.

---
+++ cmake/arm-none-eabi/arm-none-eabi.cmake
@@ -0,0 +1,15 @@
+message(STATUS "--- LOADING ARM QEMU TOOLCHAIN ---")
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+find_program(CMAKE_C_COMPILER arm-none-eabi-gcc REQUIRED)
+find_program(CMAKE_CXX_COMPILER arm-none-eabi-g++ REQUIRED)
+
+set(ARM_FLAGS "-mcpu=cortex-m3 -mthumb")
+set(CMAKE_C_FLAGS_INIT "${ARM_FLAGS}")
+set(CMAKE_CXX_FLAGS_INIT "${ARM_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "${ARM_FLAGS} --specs=rdimon.specs -lc -lrdimon")
+
+set(CMAKE_CROSSCOMPILING_EMULATOR qemu-arm-static -cpu cortex-m3 -L /usr/arm-none-eabi)
