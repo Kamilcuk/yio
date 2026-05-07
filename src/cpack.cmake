ad7dc0f5453eb390ffe4d2a73fa9e8c95b85faf6
Kamil <kamil@example.com>
2026-05-07 11:25:48 +0200

    cpack: update

--- src/cpack.cmake
+++ src/cpack.cmake
@@ -1,22 +1,25 @@
-
-set(CPACK_PACKAGE_VENDOR "Kamil Cukrowski")
+set(CPACK_PACKAGE_VENDOR "Kamil Cukrowski <kamilcukrowski@gmail.com>")
+set(CPACK_PACKAGE_CONTACT "Kamil Cukrowski <kamilcukrowski@gmail.com>")
 set(CPACK_PACKAGE_DESCRIPTION "YIO Iconic Output C Library that aims in type-safe printf replacement")
 set(CPACK_PACKAGE_HOMEPAGE_URL "https://gitlab.com/kamcuk/yio")
+set(CPACK_PACKAGE_VERSION "${YIO_PROJECT_VERSION}")
+if(YIO_PROJECT_VERSION MATCHES "([0-9]+)\\.([0-9]+)\\.([0-9]+)")
+	set(CPACK_PACKAGE_VERSION_MAJOR "${CMAKE_MATCH_1}")
+	set(CPACK_PACKAGE_VERSION_MINOR "${CMAKE_MATCH_2}")
+	set(CPACK_PACKAGE_VERSION_PATCH "${CMAKE_MATCH_3}")
+endif()
+
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_LIST_DIR}/../LICENSE")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_LIST_DIR}/../README.md")
 set(CPACK_STRIP_FILES TRUE)
-set(CPACK_PACKAGE_CONTACT "Kamil Cukrowski <kamilcukrowski@gmail.com>")
-set(CPACK_PACKAGE_VENDOR "Kamil Cukrowski <kamilcukrowski@gmail.com>")
 
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6")
-set(CPACK_DEBIAN_PACKAGE_RECOMMENDS "libunistring2")
+# Debian
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
 
+# RPM
 set(CPACK_RPM_COMPONENT_INSTALL ON)
 set(CPACK_RPM_PACKAGE_LICENSE "GPL3.0")
 set(CPACK_RPM_PACKAGE_URL "${CPACK_PACKAGE_HOMEPAGE_URL}")
-set(CPACK_RPM_PACKAGE_REQUIRES "libunistring")
-set(CPACK_RPM_PACKAGE_SUGGESTS "libunistring")
 set(CPACK_RPM_PACKAGE_PROVIDES "yio")
 
 include(CPack)
-
